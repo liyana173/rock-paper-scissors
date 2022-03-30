c610dca06484f0f0d0e5d8c5b29fb0bf9883ad94
Nurliyana <nurliyana173@gmail.com>
2022-03-30 19:19:36 +0800

    Add playRound function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -8,4 +8,66 @@ function computerPlay() {
     return choices.at(randomIndex);
 }
 
-console.log(computerPlay())
+function playRound(playerSelection, computerSelection) {
+    //if computerSelection is rock, go through all playerSelection choices
+    //if computerSelection is paper, go through all playerSelection choices
+    //if computerSelection is scissors, go through all playerSelection choices
+    let result;
+
+    if (computerSelection == 'rock') {
+        switch (playerSelection.toLowerCase()) {
+            case 'rock':
+                result = 'tie';
+                return ('Tie!');
+                break;
+            case 'paper':
+                result = 'win';
+                return ('You win! Paper beats rock!');
+                break;
+            case 'scissors':
+                result = 'lose';
+                return ('You lose! Rock beats scissors!');
+                break;
+        }
+    }
+
+    else if (computerSelection == 'paper') {
+        switch (playerSelection.toLowerCase()) {
+            case 'rock':
+                result = 'lose';
+                return ('You lose! Paper beats rock!');
+                break;
+            case 'paper':
+                result = 'tie';
+                return ('Tie!');
+                break;
+            case 'scissors':
+                result = 'win';
+                return ('You win! Scissors beat paper!');
+                break;
+        }
+    }
+
+    else {
+        switch (playerSelection.toLowerCase()) {
+            case 'rock':
+                result = 'win';
+                return ('You win! Rock beats scissors!');
+                break;
+            case 'paper':
+                result = 'lose';
+                return ('You lose! Scissors beat paper!');
+                break;
+            case 'scissors':
+                result = 'tie';
+                return ('Tie!');
+                break;
+        }
+    }
+}
+
+
+let playerSelection = 'rock';
+let computerSelection = computerPlay();
+console.log(computerSelection)
+console.log(playRound(playerSelection, computerSelection))
